e9f7262507faa1089c3b6a0fc27979bdaa6ea85d
RyanDur <ryan.p.durling@gmail.com>
2013-06-21 00:10:59 -0700

    it works

--- stickyNav/scripts/myScript.js
+++ stickyNav/scripts/myScript.js
@@ -1,10 +1,7 @@
 $(function() {
-    $('a').text('xJQuery').removeClass("blue")
-    .addClass("bold")
-    .css("color", "blue");
-
-    $('a').click(function(event) {
-        event.preventDefault();
-        $(this).fadeToggle('slow');
-    });
+  $(".nav-container").waypoint({
+    handler: function(direction) {
+      $("nav").toggleClass('sticky', direction=='down');
+    }
+  });
 });
